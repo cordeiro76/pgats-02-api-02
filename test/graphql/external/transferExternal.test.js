bf863a9128a4780ade6737f65bafdb5322184b07
Jessica <cordeiro76@hotmail.com>
2025-09-06 15:43:51 -0300

    Adicionando novos testes automatizados

---
+++ test/graphql/external/transferExternal.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest');
+const { expect, use } = require('chai');
+
+const chaiExclude = require('chai-exclude');
+use(chaiExclude);
+
+describe('Testes de Transferência', () => {
+
+    before(async () => {
+        const loginUser = require('../fixture/requisicoes/login/loginUser.json');
+        const resposta = await request('http://localhost:4000/graphql')
+            .post('')
+            .send(loginUser);
+
+        token = resposta.body.data.loginUser.token;
+
+    });
+
+    beforeEach(() => {
+        createTransfer = require('../fixture/requisicoes/tranferencia/createTransfer.json');
+    })
+
+    it('Validar que é possível transferir grana entre duas contas', async () => {
+        const respostaEsperada = require('../fixture/respostas/trasferencia/validarQueEPossivelTransferirGranaEntreDuasContas.json')
+        const respostaTransferencia = await request('http://localhost:4000/graphql')
+            .post('')
+            .set('Authorization', `Bearer ${token}`)
+            .send(createTransfer);
+
+        expect(respostaTransferencia.status).to.equal(200);
+        //expect(respostaTransferencia.body).to.eql(respostaEsperada);
+        expect(respostaTransferencia.body.data.createTransfer)
+            .excluding('date')
+            .to.deep.equal(respostaEsperada.data.createTransfer)
+    });
+
+    it('Validar que não é possível transferir de uma conta que não possui saldo suficiente', async () => {
+        createTransfer.variables.value = 10000.01
+        const respostaTransferencia = await request('http://localhost:4000/graphql')
+
+            .post('')
+            .set('Authorization', `Bearer ${token}`)
+            .send(createTransfer);
+
+        expect(respostaTransferencia.body.errors[0].message).to.equal('Saldo insuficiente');
+    });
+
+});
